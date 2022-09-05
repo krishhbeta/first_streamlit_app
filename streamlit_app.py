5f302ef162c182fa1b66634d89665140527a26be
krishhbeta <112463806+krishhbeta@users.noreply.github.com>
2022-09-05 16:58:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_fro_function=get_fruityvice_data(fruit_choice)
+    back_from_function=get_fruityvice_data(fruit_choice)
   
  
     # write your own comment - what does this do?
